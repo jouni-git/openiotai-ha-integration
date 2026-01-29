70234a2f8a910354adba58a0f84cd1900ae0febc
Jouni Tiilikainen <jouni.tiilikainen@yahoo.com>
2026-01-29 17:21:57 +0200

    Fix config entry setup for new Home Assistant API

--- custom_components/openiotai/__init__.py
+++ custom_components/openiotai/__init__.py
@@ -22,8 +22,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up OpenIOTAI from a config entry."""
     _LOGGER.info("Setting up OpenIOTAI config entry")
 
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
